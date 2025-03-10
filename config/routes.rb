f261c4cccbe77633789baf372c79d16d5b7cc384
EC2 Default User <ec2-user@ip-172-31-6-245.ap-northeast-1.compute.internal>
2025-03-10 07:56:32 +0000

    完成

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
-  get 'lists/new'
-  get 'lists' => 'lists#new'
-  post 'lists' => 'lists#create'
-  get 'lists/:id' => 'lists#show', as: 'list'
-  get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
-  get 'homes/top'
-  patch 'lists/:id' => 'lists#update', as: 'update_list'
-  delete 'lists/:id' => 'lists#destroy', as: 'destroy_list'
+  root to: "homes#top"
+  resources :books, only: [:index, :create, :show, :edit, :update, :destroy]
 end
